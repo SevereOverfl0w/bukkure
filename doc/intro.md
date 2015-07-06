4d69d1d9f7b4552cc11b134080435b0104a2d1f6
SevereOverfl0w <overfl0w@narna.co>
2015-07-06 09:17:58 +0100

    First.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljbukkit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljbukkit "0.1.0-SNAPSHOT"
+  :description "Bringing Clojure to the Bukkit API"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
