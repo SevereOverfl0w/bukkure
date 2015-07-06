23fb0373e5841c04919147854cd6137b1ac74797
SevereOverfl0w <overfl0w@narna.co>
2015-07-06 10:54:53 +0100

    add localrepo as a plugin.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-source-paths ["javasrc"]
+  :javac-options ["-d" "classes/" "-source" "1.7" "-target" "1.7"]
+  :resource-paths ["resources/*"]
+  :plugins [[lein-localrepo "0.5.3"]]
   :dependencies [[org.clojure/clojure "1.7.0"]])
