99712e17b40ef6184d83fd68af483823d8516310
SevereOverfl0w <overfl0w@narna.co>
2015-07-06 09:34:21 +0100

    add a quick utility for updating files.

---
+++ utils/namespace-todo.py
@@ -0,0 +1,16 @@
+# Converts the cljminecraft namespace to cljbukkit,
+# Then adds a TODO, for me to check the file out later.
+
+import sys
+
+fileLocations = sys.argv[1:]
+
+for fileLoc in fileLocations:
+    myfile = open(fileLoc, "r")
+
+    contents = myfile.read()
+    contents = contents.replace("cljminecraft", "cljbukkit")
+    contents = ";; TODO: Check this file manually\n" + contents
+
+    myfileW = open(fileLoc, "w")
+    myfileW.write(contents)
