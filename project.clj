7fd1cb6dc875f137698dee78deea149d31e4d2e5
Dominic <dominic@juxt.pro>
2016-05-31 21:19:00 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bukkure "0.5+alpha"
+(defproject bukkure "1.6+alpha"
   :description "Bringing Clojure to the Bukkit API"
   :url "http://github.com/SevereOverfl0w/Bukkure"
   :license {:name "Eclipse Public License"
