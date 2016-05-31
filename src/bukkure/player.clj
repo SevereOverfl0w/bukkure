3f86aa9cd419c1c8cffabcbc5ee6ffa8656be506
Dominic <dominic@juxt.pro>
2016-05-31 21:18:29 +0100

    Fix for 1.9

--- src/bukkure/player.clj
+++ src/bukkure/player.clj
@@ -33,7 +33,6 @@
 (event-player inventory.inventory-click)
 (event-player inventory.inventory-close)
 (event-player inventory.inventory-open)
-(event-player painting.painting-place)
 (event-player player.player)
 
 (extend-type org.bukkit.inventory.InventoryView
